9b6fd85341d4520ccae32344b15547d545fce4b2
Athichart Tangpong <victorybattleship@gmail.com>
2024-04-13 01:56:49 -0700

    Add zipall.py and its pytest

---
+++ src/test/__init__.py
@@ -0,0 +1,6 @@
+# This file is required for pytest to pass. Without this file, test_zipall.py,
+# for example, cannot import zipall.py
+#
+# Reference:
+# https://www.youtube.com/watch?v=oiPkLyW3WwE
+# https://docs.pytest.org/en/7.1.x/explanation/goodpractices.html
