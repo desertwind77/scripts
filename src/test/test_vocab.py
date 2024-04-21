5da56085135c222be46bae00c2bf3d4d7182cc7e
Athichart Tangpong <victorybattleship@gmail.com>
2024-04-21 00:36:59 -0700

    Rewrite vocab to make the game interactive, add a function to verify the dictionary file in vocab.py, add test_vocab.py

--- src/test/test_vocab.py
+++ src/test/test_vocab.py
@@ -1,6 +1,44 @@
 #!/usr/bin/env python3
 '''Test file for the vocab.py script'''
 
+import os
+import pytest
+from vocab import Dictionary
 
-def test_main():
-    '''A place holder'''
+
+@pytest.fixture()
+def filename():
+    '''Return the location of the dictionary file'''
+    script_path = os.path.realpath(os.path.dirname(__file__))
+    return os.path.join(script_path, '../config/vocabulary.yaml')
+
+
+@pytest.fixture()
+def dictionary(filename):
+    '''Return the dictionary object'''
+    return Dictionary(filename)
+
+
+def test_print_all(dictionary):
+    '''Print the whole dictionary'''
+    words = dictionary.select_words(all_words=True)
+    dictionary.print_word(words)
+
+
+def test_print_non_exist_word(dictionary):
+    '''Search for a non-existent word'''
+    words = ['NON_EXIST_WORD']
+    dictionary.print_word(words)
+
+
+def test_game_correct(dictionary):
+    '''Answer a game correctly'''
+    game = dictionary.generate_game()
+    assert game.check_answer(game.answer)
+
+
+def test_game_incorrect(dictionary):
+    '''Answer a game incorrectly'''
+    game = dictionary.generate_game()
+    choice = next(i[0] for i in game.multiple_choices if i[1] != game.answer)
+    assert not game.check_answer(choice)
--- src/vocab.py
+++ src/vocab.py
@@ -3,26 +3,20 @@
 A script to randomly print a word from the custom dictionary
 
 TODO:
-- pyest
-- Continuous game
+- Clean up the dictionary file
+- Print the game statistics
 - Python GUI
 - Mobile App
 - Native Mac App
 '''
-from collections import namedtuple
+from dataclasses import dataclass
 import argparse
 import random
 import re
-
+import sys
 # pylint: disable=import-error
 from colorama import Fore, Style
 import yaml
-
-from utils import check_exists, get_abs_path
-
-CONFIG_FILENAME = 'config/vocabulary.yaml'
-
-Pattern = namedtuple('Pattern', ['src', 'dst'])
 
 
 class RegexMatchFailure(Exception):
@@ -33,137 +27,297 @@ class NoExampleFound(Exception):
     '''No example presents for this word's meaning'''
 
 
-def load_vocabuary(filename):
-    '''Load the vocabuary from a file'''
-    filename = get_abs_path(__file__, filename)
-    check_exists(filename, is_file=True)
-
-    output = None
-    with open(filename, 'r', encoding='utf-8') as yaml_file:
-        output = yaml.safe_load(yaml_file)
-    return output
+@dataclass
+class ReplacePattern:
+    '''For replacing src with dst'''
+    src: str
+    dst: str
 
 
-def sanitize_text(txt):
-    '''Clean up text e.g. removing escape characters'''
-    replacement = [
-        Pattern(r'\(', '('),
-        Pattern(r'\)', ')'),
-        Pattern('\\"', '"'),
-        Pattern('\\"', '"'),
-        Pattern('{', ''),
-        Pattern('}', ''),
-    ]
-    for pattern in replacement:
-        txt = txt.replace(pattern.src, pattern.dst)
-    return txt
-
-
-def select_words(vocab, all_words=False):
-    '''Select words from the dictionary'''
-    words = []
-    keys = sorted(vocab.keys())
-    if all_words:
-        words = keys
-        print(f'Dictionary size = { len( words ) } words')
-    else:
-        index = random.randint(0, len(keys) - 1)
-        words = [keys[index]]
-    return words
-
-
-def print_word(vocab, words=None):
-    '''Print all words in the list'''
+class Word:
     tab = '   '
-    for word in words:
-        if word not in vocab:
-            continue
-        print(Fore.RED + f'{sanitize_text( word )}:')
-        for meaning, examples in vocab[word].items():
-            print(Fore.GREEN + f'{tab}{sanitize_text( meaning ) }:')
-            if not examples:
+
+    '''General base class'''
+    def sanitize_text(self, txt: str) -> str:
+        '''Clean up text e.g. removing escape characters
+
+        Args:
+            txt (str): the text to be cleaned up
+
+        Returns:
+            (str) the cleaned up text
+        '''
+        replacement = [
+            ReplacePattern(r'\(', '('),
+            ReplacePattern(r'\)', ')'),
+            ReplacePattern('\\"', '"'),
+            ReplacePattern('\\"', '"'),
+            ReplacePattern('{', ''),
+            ReplacePattern('}', ''),
+        ]
+        for pattern in replacement:
+            txt = txt.replace(pattern.src, pattern.dst)
+        return txt
+
+
+class Game(Word):
+    '''A word-filing game'''
+    def __init__(self, word: str, meaning: str,
+                 example: str, choices: list[str]) -> None:
+        '''Constructor
+
+        Args:
+            word (str): the word and correct answer
+
+            meaning (str): the meaning of the word
+
+            example (str): the example sentence to be used as the question
+
+            choices (list[str]): a list of words to be used in the multiple choices
+        '''
+        # Replace the word between { and } with underscores.
+        example = example.replace(r'\{', '{')
+        example = example.replace(r'\}', '}')
+        obj = re.search(r'.*({.*}).*', example)
+        if not obj:
+            raise RegexMatchFailure(example)
+        pattern = obj.group(1)
+        example = example.replace(pattern, '_' * 5)
+
+        self.word = self.sanitize_text(word)
+        self.meaning = self.sanitize_text(meaning)
+        self.example = self.sanitize_text(example)
+        self.choices = [ self.sanitize_text(t) for t in choices]
+
+        letters = [ chr(ord('a') + i ) for i in range(len(self.choices))]
+        self.multiple_choices = list(zip( letters, self.choices))
+        self.answer = next(i[0] for i in self.multiple_choices if i[1] == self.word)
+
+    def print(self) -> None:
+        '''Print the question, meaning and multiple choices'''
+        print(f'Question : {self.example}')
+        print(f'Meaning  : {self.meaning}')
+        for choice in self.multiple_choices:
+            print(f'{self.tab}{choice[0]}) {choice[1]}')
+
+    def check_answer(self, answer: str) -> bool:
+        '''Check if the answer is correct
+
+        Args:
+            answer (str): the answer
+
+        Returns:
+            (bool) True if the answer is correct
+        '''
+        return answer == self.answer
+
+class Dictionary(Word):
+    '''The class representing the dictionary'''
+    color_word = Fore.RED
+    color_meaning = Fore.GREEN
+    color_example = Fore.CYAN
+    color_highlight = Fore.YELLOW
+    num_multiple_choices = 4
+
+    def __init__(self, filename: str) -> None:
+        '''Constructor
+
+        Args:
+            filename (str): the dictionary file in the YAML format
+        '''
+        self.filename = filename
+        self.dictionary = self.load_from_file()
+
+    def size(self) -> int:
+        '''Get the number of words in the dictionary
+
+        Returns:
+            (int) the number of words in the dictionary
+        '''
+        return 0 if self.dictionary is None else len(self.dictionary)
+
+    def load_from_file(self) -> dict:
+        '''Load the vocabuary from a YAML file
+
+        Args:
+            filename (str) : the YAML file containing the vocabuary
+
+        Returns:
+            dictionary containing the vocabuary in the following format
+
+                {
+                    'vocab1' : {
+                        'meaning1' : [ 'example1', 'example2', ... ],
+                        'meaning2' : [ 'example1', 'example2', ... ],
+                        ...
+                    },
+                }
+        '''
+        output = None
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as yaml_file:
+                output = yaml.safe_load(yaml_file)
+        except FileNotFoundError:
+            print(f'File not found: {self.filename}')
+            sys.exit(1)
+        return output
+
+    def select_words(self, all_words: bool = False) -> list[str]:
+        '''Randomly select a  word or all words from the dictionary for printing
+        depending on the all_words flag.
+
+        Args:
+            dictionary (dict): the dictionary containing vocabularies, meanings,
+                               and examples
+            all_words (bool): select all the words in the dictionary
+
+        Returns:
+            list of selected words
+        '''
+        if all_words:
+            words = sorted(self.dictionary.keys())
+        else:
+            words = [random.choice(list(self.dictionary.keys()))]
+        return words
+
+
+    def print_word(self, words: list[str] = None) -> None:
+        '''Look up and print all the words in the dictionary.
+
+        Args:
+            dictionary (dict): the dictionary containing vocabularies, meanings,
+                               and examples
+
+            words (list[str]): list of words
+        '''
+        for word in words:
+            if word not in self.dictionary:
                 continue
-            for example in examples:
-                if r'\{' in example and r'\}' in example:
+            print(self.color_word, end='')
+            print(f'{self.sanitize_text(word)}:')
+
+            for meaning, examples in self.dictionary[word].items():
+                print(self.color_meaning, end='')
+                print(f'{self.tab}{self.sanitize_text(meaning) }:')
+
+                if not examples:
+                    continue
+
+                for example in examples:
                     example = example.replace(r'\{', '{')
                     example = example.replace(r'\}', '}')
-                begin_index = example.find('{')
-                end_index = example.find('}')
-                if begin_index == -1 or end_index == -1:
-                    print(Fore.CYAN + f'{tab}{tab}{sanitize_text( example ) }')
-                else:
-                    begin = sanitize_text(example[0: begin_index])
-                    middle = sanitize_text(example[begin_index: end_index + 1])
-                    end = sanitize_text(example[end_index + 1:])
-                    print(f'{tab}{tab}', end='')
-                    print(Fore.CYAN + f'{begin}', end='')
-                    print(Fore.YELLOW + f'{middle}', end='')
-                    print(Fore.CYAN + f'{end}')
-    print(Style.RESET_ALL)
 
+                    begin_index = example.find('{')
+                    end_index = example.find('}')
+                    if begin_index == -1 or end_index == -1:
+                        print(self.color_example, end='')
+                        print(f'{self.tab}{self.tab}{self.sanitize_text(example) }')
+                    else:
+                        begin = self.sanitize_text(example[0: begin_index])
+                        middle = self.sanitize_text(example[begin_index: end_index + 1])
+                        end = self.sanitize_text(example[end_index + 1:])
 
-def game_fill_in_word(vocab):
-    '''Game to fill a word in the blank'''
-    def get_word(vocab):
-        word, word_info = random.choice(list(vocab.items()))
-        # For debugging:
-        word_info = vocab[word]
-        meaning = random.choice(list(word_info.keys()))
-        examples = word_info[meaning]
+                        print(f'{self.tab}{self.tab}', end='')
+                        print(self.color_example + f'{begin}', end='')
+                        print(self.color_highlight + f'{middle}', end='')
+                        print(self.color_example + f'{end}')
+        print(Style.RESET_ALL)
+
+    def generate_game(self) -> Game:
+        '''Generate a word-filling game'''
+        # Select word, meaning, example
+        word, word_info = random.choice(list(self.dictionary.items()))
+        meaning, examples = random.choice(list(word_info.items()))
+        if not examples:
+            raise NoExampleFound(word)
         example = random.choice(examples) if examples else None
-        return word, meaning, example
 
-    def get_multiple_choices(vocab, word):
-        # Select three multiple choices
-        cindices = list(random.sample(range(0, len(vocab) - 1), 4))
-        choices = [sorted(vocab.keys())[i] for i in cindices]
-        if word not in choices:
-            choices.append(word)
+        # Generate the multiple choices
+        cindices = list(random.sample(range(0, len(self.dictionary) - 1),
+                                      self.num_multiple_choices + 1))
+        choices = [sorted(self.dictionary.keys())[i] for i in cindices]
+        if word in choices:
+            choices = [c for c in choices if c != word]
+        else:
+            choices = choices[1:]
+        choices = [*choices, word]
         random.shuffle(choices)
-        return choices
 
-    word, meaning, example = get_word(vocab)
-    if not example:
-        raise NoExampleFound(word)
-    choices = get_multiple_choices(vocab, word)
+        return Game(word, meaning, example, choices)
 
-    obj = re.search(r'.*({.*}).*', example)
-    if not obj:
-        raise RegexMatchFailure(example)
-    pattern = obj.group(1)
-    blank = '_' * len(pattern)
-    example = example.replace(pattern, blank)
-    print(f'Question : {sanitize_text( example )}\n')
-    print(f'Meaning  : {sanitize_text( meaning )}\n')
-    print('Answer   :')
-    for choice in choices:
-        print(f'* {sanitize_text( choice )}')
+    def verify(self) -> None:
+        '''Verify that all meanings have at least an example'''
+        need_examples = set(word
+                            for word, meanings in self.dictionary.items()
+                            for meaning, examples in meanings.items()
+                            if examples is None)
+        for word in sorted(list(need_examples)):
+            self.print_word([word])
+
+        if need_examples:
+            assert False
 
 
-def process_arguments():
-    '''Process the command line arguments'''
+def process_arguments() -> argparse.Namespace:
+    '''Process the command line arguments
+
+    Returns:
+        (argparse.Namespace) parsed command line arguments
+    '''
     parser = argparse.ArgumentParser()
+    parser.add_argument('dictionary', action='store',
+                        help='The dictionary file in the YAML format')
     parser.add_argument('-a', '--all', action='store_true',
                         help='Print all words in the dictionary')
     parser.add_argument('-g', '--game', action='store_true',
                         help='Play the game to fill in the blank')
     parser.add_argument('-s', '--search', action='store',
                         help='Search the word')
+    parser.add_argument('-V', '--verify', action='store_true',
+                        help='Verify the dictionary file')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Print debug information')
     return parser.parse_args()
 
 
 def main():
     '''The main function'''
     args = process_arguments()
-    vocab = load_vocabuary(CONFIG_FILENAME)
+    dictionary = Dictionary(args.dictionary)
+
+    if args.verify:
+        dictionary.verify()
+    if args.verbose:
+        print(f'Dictionary size = {dictionary.size()} words')
 
     if args.game:
-        game_fill_in_word(vocab)
-    elif args.search:
-        print_word(vocab, [args.search])
+        game = dictionary.generate_game()
+        game.print()
+
+        attempt, max_attempt, new_game = 0, 2, False
+        answer = input('Enter your answer or q/Q to quite:')
+        while answer not in ['q', 'Q']:
+            if game.check_answer(answer):
+                print(Fore.GREEN + "That's correct! Congratulation" + Style.RESET_ALL)
+                new_game = True
+            else:
+                attempt += 1
+                if attempt == max_attempt:
+                    print(Fore.RED + "That's too bad!" + Style.RESET_ALL)
+                    new_game = True
+                else:
+                    print(Fore.RED + "Try again!" + Style.RESET_ALL)
+                    answer = input('Enter your answer or q/Q to quite:')
+
+            if new_game:
+                attempt, new_game = 0, False
+                game = dictionary.generate_game()
+                game.print()
+                answer = input('Enter your answer or q/Q to quite:')
     else:
-        words = select_words(vocab, all_words=args.all)
-        print_word(vocab, words)
+        words = [args.search] if args.search else \
+                dictionary.select_words(all_words=args.all)
+        dictionary.print_word(words)
 
 
 if __name__ == '__main__':
