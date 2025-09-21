53350e5d80cf79fd227055266f093ec54e384f50
Athichart Tangpong <victorybattleship@gmail.com>
2025-09-20 23:19:39 -0700

    Restructure mosaic.py

--- src/mosaic.py
+++ src/mosaic.py
@@ -8,36 +8,159 @@ import random
 import sys
 
 # pylint: disable=import-error
-from PIL import Image, ImageChops
-import numpy as np
+from PIL import Image, ImageChops, ImageEnhance, ImageFile
+from loguru import logger
 
-class GridInfo:
-    '''Grid information'''
-    def __init__(self, image_width: int, image_height: int,
-                 num_horizontal_grids: int, num_vertical_grids: int) -> None:
-        self.image_width = image_width
-        self.image_height = image_height
-        self.num_horizontal_grids = num_horizontal_grids
-        self.num_vertical_grids = num_vertical_grids
-        self.grid_width = image_width // num_horizontal_grids
-        self.grid_height = image_height // num_vertical_grids
+EXPERIMENTAL = False
 
-# Flag to enable debugging
-DEBUG_ENABLED = True
+class MosaicGenerator:
+    def __init__(self, target_image_file: str, source_image_dir: str, grid: int) -> None:
+        self.target_image_file = target_image_file
+        self.source_image_dir = source_image_dir
+        self.grid = grid
 
-def debug(txt):
-    '''Print debug message'''
-    if not DEBUG_ENABLED:
-        return
-    print(txt)
+        self.target_image = self.load_target_image()
+        self.image_width = self.target_image.size[0]
+        self.image_height = self.target_image.size[1]
+        self.grid_size = min(self.image_width, self.image_height) // grid
+        self.horizontal_grid = math.ceil(self.image_width/self.grid_size)
+        self.vertical_grid = math.ceil(self.image_height/self.grid_size)
+
+    def load_target_image(self) -> ImageFile.ImageFile:
+        '''Load the target image file'''
+        logger.debug(f'Loading target image {self.target_image_file}')
+        try:
+            image = Image.open(self.target_image_file)
+        except FileNotFoundError:
+            print(f'Unable to open {self.target_image_file}')
+            sys.exit(1)
+        return image
+
+    def load_source_image_folder(self) -> list[ImageFile.ImageFile]:
+        '''Load source images to be used as the background image'''
+        logger.debug(f'Loading source images from {self.source_image_dir}')
+
+        result = []
+        source_image_path = Path(self.source_image_dir)
+        for file in source_image_path.glob('**/*.jpg'):
+            try:
+                with open(file, 'rb') as f:
+                    # Open and load the image into memory
+                    image = Image.open(f)
+                    image.load()
+                    result.append(image)
+            except KeyboardInterrupt:
+                # If the number of images in the folder is huge, this function may
+                # take too long. Let's exit the program gracefully.
+                print('Keyboard interrupt')
+                sys.exit(1)
+        return result
+
+    def resize_source_images(self, images) -> list[ImageFile.ImageFile]:
+        '''Crop and resize the images in the list to a square'''
+        logger.debug('Resizing the source images')
+        cropped_image_list = []
+        for image in images:
+            image_size = min(image.size[0], image.size[1])
+            crop_box = (0, 0, image_size, image_size)
+            new_size = (self.grid_size, self.grid_size)
+            tmp_image = image.crop(crop_box)
+            tmp_image = tmp_image.resize(new_size)
+            cropped_image_list.append(tmp_image)
+        return cropped_image_list
+
+    def reduce_image_opacity(self, image: Image.Image,
+                             opacity_percentage: int) -> Image.Image:
+        '''Reduce the opacity of an image'''
+        logger.debug(f'Adjust the opacity to {opacity_percentage}')
+        opacity = opacity_percentage / 100.0
+        rgba_img = image.convert('RGBA')
+        # red, green, blue, alpha
+        _, _, _, alpha = rgba_img.split()
+        alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+        rgba_img.putalpha(alpha)
+        return rgba_img.convert('RGB')
+
+    def adjust_image_brightness(self, image: Image.Image,
+                                brightness_factor: float) -> Image.Image:
+        '''Adjust the brightness of an image. At brightness factor 1.5, the
+        image will be 50% brighter.'''
+        logger.debug(f'Adjust the brightness level to {brightness_factor}')
+        enhancer = ImageEnhance.Brightness(image)
+        enhanced_image = enhancer.enhance(brightness_factor)
+        return enhanced_image
+
+    def create_background_image(self) -> Image.Image:
+        '''Create the background image'''
+        logger.debug('Creating the background image')
+        # Load source images to be used as the background image
+        source_images = self.load_source_image_folder()
+        # Shuffle the source images so that each run returns a different output
+        random.shuffle(source_images)
+        # Crop and resize the source images into squares
+        cropped_image_list = self.resize_source_images(source_images)
+        # Create the background image list
+        num_src_img = len(cropped_image_list)
+        num_grids = self.horizontal_grid * self.vertical_grid
+        img_indexes = [i % num_src_img for i in range(num_grids)]
+        background_image_list = [cropped_image_list[i] for i in img_indexes]
+        # Create an empty image to store the final mosaic image
+        mosaic_image = Image.new('RGB', (self.image_width, self.image_height))
+        # Copy the background images onto each grid in the mosaic image
+        for index, tile in enumerate(background_image_list):
+            row = index // self.horizontal_grid
+            col = index - (row * self.vertical_grid)
+            mosaic_image.paste(tile, (col * self.grid_size,
+                                      row * self.grid_size))
+        return mosaic_image
+
+    def create_mosaic(self, output_filename: str,
+                      opacity_percentage: int = 100,
+                      brightness_factor: float = 1.0) -> Image.Image:
+        '''Create the final mosaic image'''
+        background_image = self.create_background_image()
+        if EXPERIMENTAL:
+            background_image = self.reduce_image_opacity(background_image,
+                                                         opacity_percentage)
+            background_image = self.adjust_image_brightness(background_image,
+                                                            brightness_factor)
+
+        logger.debug(f'Creating {output_filename}')
+        output_image = ImageChops.soft_light(self.target_image, background_image)
+        output_image.save(output_filename, 'PNG')
+        return output_image
+
+def validate_range(value_str: str, min_val: float | None = None,
+                   max_val: float | None = None) -> float:
+    '''Validate that value is between min_val and max_val (inclusive)'''
+    try:
+        value = float(value_str)
+    except ValueError as exc:
+        msg = f"'{value_str}' is not a valid integer."
+        raise argparse.ArgumentTypeError(msg) from exc
+    min_value = -math.inf if min_val is None else min_val
+    max_value = math.inf if max_val is None else max_val
+    if not min_value <= value <= max_value:
+        msg = f'Value must be between [{min_val}, {max_val}]'
+        raise argparse.ArgumentTypeError(msg)
+    return value
 
 def parse_argument():
     '''Parse the command line arguments'''
-    default_grid = (64, 64)
     parser = argparse.ArgumentParser(description='Create a photomasaic')
-    parser.add_argument('-g', '--grid', nargs=2, default=default_grid,
-                        help='number of grids in (width, height). '
-                        'The default value is (64, 64)')
+    parser.add_argument('-B', '--brightness', action='store', default=1,
+                        type=lambda x: validate_range(x, 0.1, 2),
+                        help='Opacity of the background image')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Print debug messages')
+    parser.add_argument('-g', '--grid', default=64,
+                        # Set the minimum number of grids to 16
+                        type=lambda x: validate_range(x, 16 ),
+                        help='number of grids on the shorter side of '
+                        'the target image (default=64).')
+    parser.add_argument('-O', '--opacity', action='store', default=100,
+                        type=lambda x: validate_range(x, 0, 100),
+                        help='Opacity of the background image')
     parser.add_argument('-o', '--output', default='mosaic_output.png',
                         help='output image')
     parser.add_argument('-s', '--show', action='store_true',
@@ -46,148 +169,22 @@ def parse_argument():
     parser.add_argument('source', help='source folder for the input images')
     return parser.parse_args()
 
-def load_image(filename: str) -> Image:
-    '''Load the target image
-
-    Parameters:
-        filename (str): the location of the target image
-
-    Return:
-        A Pillow image of the target image
-    '''
-    try:
-        debug(f'Loading target {filename}')
-        image = Image.open(filename)
-    except FileNotFoundError:
-        print(f'Unable to get source images from {filename}')
-        sys.exit(1)
-    return image
-
-def get_grid_info(image: Image, num_horizontal_grids: int,
-                  num_vertical_grids: int) -> GridInfo:
-    '''Find out the information of a grid once we split the target image
-
-    Parameters:
-        image (Image): pillow Image object to be split
-        horizontal_grid (int): number of horizontal grids
-        vertical_grid (int): number of vertical grids
-
-    Return:
-        a Grid object containing the information of a grid
-    '''
-    return GridInfo(image.size[0], image.size[1], num_horizontal_grids,
-                    num_vertical_grids)
-
-def load_source_image_folder(source: str) -> list[Image]:
-    '''Get the source images to be used to create a mosaic
-
-    Parameters:
-        target_image (Image): the target image
-        source (str): the folder that contains the source images
-
-    Return:
-        A list of pillow Images object of the source images or None
-    '''
-    debug(f'Loading source {source}')
-
-    result = []
-    source_image_path = Path(source)
-    for file in source_image_path.glob('**/*.jpg'):
-        try:
-            with open(file, 'rb') as f:
-                # Open and load the image into memory
-                image = Image.open(f)
-                image.load()
-                result.append(image)
-        except KeyboardInterrupt:
-            # If the number of images in the folder is huge, this function may
-            # take too long. Let's exit the program gracefully.
-            print('Keyboard interrupt')
-            sys.exit(1)
-    return result
-
-def create_background_image(source_image_list: list[Image],
-                            grid_info: GridInfo) -> Image:
-    '''Combine grid of images to an image
-
-    Parameters:
-        source_image_list(list[Image]): source images in PILLOW objects
-        grid_info (GridInfo): information of the grids
-
-    Return:
-        A mosaic image constructed from the source images
-    '''
-    # Shuffle the source images so that each run returns a different output
-    random.shuffle(source_image_list)
-
-    # Resize the source images
-    debug('Resize the source images')
-    for image in source_image_list:
-        # TODO: handle different image size better
-        image.thumbnail((grid_info.grid_width, grid_info.grid_height))
-
-    num_source_images = len(source_image_list)
-    total_grids = grid_info.num_horizontal_grids * grid_info.num_vertical_grids
-    source_image_indexes = []
-
-    cur_index = 0
-    for _ in range(total_grids):
-        source_image_indexes.append(cur_index)
-        cur_index = (cur_index + 1) % num_source_images
-
-    source_image_tiles = [source_image_list[i] for i in source_image_indexes]
-
-    mosaic_image = Image.new('RGB', (grid_info.image_width, grid_info.image_height))
-
-    for index, tile in enumerate(source_image_tiles):
-        row = index // grid_info.num_horizontal_grids
-        col = index - (row * grid_info.num_vertical_grids)
-        mosaic_image.paste(tile, (col*grid_info.grid_width,
-                                  row*grid_info.grid_height))
-
-    return mosaic_image
-
-def create_and_save_mosaic_image(target_image: Image, background_image: Image,
-                                 output_filename: str) -> Image:
-    '''Create and save the final mosaic image
-
-    Argument:
-        target_image (Image): the target image
-        background_image (Image): the mosaic image created from source images
-        output_filename (str): the name of the output file
-    '''
-    # Create a mosaic image by blending the image in the soft light mode
-    output_image = ImageChops.soft_light(target_image, background_image)
-    output_image.save(output_filename, 'PNG')
-    return output_image
-
 def main():
     '''The main program'''
     args = parse_argument()
-    # Reference image
+    grid = int(args.grid)
+    opacity = int(args.opacity)
+    brightness = int(args.brightness)
     target_image_name = args.target
-    # Source images for the mosaic
     source_image_dir = args.source
-    # The output image
     output_filename = args.output
 
-    # Load the target image to a pillow image
-    target_image = load_image(target_image_name)
+    if not args.debug:
+        logger.disable('')
 
-    # Find out the information of a grid once we split the target image
-    grid_info = get_grid_info(image=target_image,
-                              num_horizontal_grids=int(args.grid[0]),
-                              num_vertical_grids=int(args.grid[1]))
+    mosaic = MosaicGenerator(target_image_name, source_image_dir, grid)
+    final_image = mosaic.create_mosaic(output_filename, opacity, brightness)
 
-    # Load all the source images into pillow objects
-    source_image_list = load_source_image_folder(source_image_dir)
-
-    # Construct a mosaic background image
-    background_image = create_background_image(source_image_list, grid_info)
-
-    # Create and save the final output image
-    final_image = create_and_save_mosaic_image(target_image, background_image,
-                                               output_filename)
     if args.show:
         final_image.show()
 
